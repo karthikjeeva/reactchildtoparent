426747e4f607204bcfb007f089816135f7431753
karthik <karthikjeeva2206@gmail.com>
2022-02-12 18:57:22 +0530

    calling child method from parent function

--- src/parent.js
+++ src/parent.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 import Child from  './child';
 
 class Parent extends Component {
+    parentfunc = () => {
+        console.log("called")
+        this.child.getAlert();
+    }
   render() {
     return (
       <div>
         <Child ref={instance => { this.child = instance; }} />
-        <button onClick={() => { this.child.getAlert(); }}>Click</button>
+        <button onClick={() => {  this.parentfunc()}}>Click</button>
       </div>
     );
   }
